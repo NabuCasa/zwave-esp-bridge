daa4e1d2ecc4cdbd7455428697bfc13b85fa1737
puddly <32534428+puddly@users.noreply.github.com>
2025-05-28 12:31:31 -0400

    Software ESP32 bootloader trigger (#10)

--- main/usb_uart_bridge_main.c
+++ main/usb_uart_bridge_main.c
@@ -16,12 +16,20 @@
 #include "esp_system.h"
 #include "esp_timer.h"
 #include "rom/ets_sys.h"
+#include "soc/rtc_cntl_reg.h"
 
 #include "tinyusb.h"
 #include "tusb.h"
 #include "tusb_cdc_acm.h"
 
 static const char *TAG = "USB2UART";
+
+// To allow entering the ESP32 bootloader *without* RTS/DTR, set these baudrates on the
+// serial port in this order. Some OSes reset the serial port to a default baudrate so
+// as long as these are opened in this order (but not necessarily sequentially), the
+// bootloader will be triggered.
+const uint32_t MAGIC_BOOTLOADER_TRIGGER_BAUDRATES[] = {150, 300, 600};
+const uint64_t MAGIC_BOOTLOADER_TRIGGER_TIMEOUT_MICROS = 5000000;
 
 #define BOARD_UART_PORT        UART_NUM_1
 #define BOARD_UART_TXD_PIN     CONFIG_BOARD_UART_TXD_PIN
@@ -32,12 +40,6 @@ static const char *TAG = "USB2UART";
 
 #define BOARD_ZG23_RESET_PIN   CONFIG_BOARD_ZG23_RESET_PIN
 #define BOARD_ZG23_BTL_PIN     CONFIG_BOARD_ZG23_BTL_PIN
-
-#ifdef CONFIG_UART_AUTO_DOWNLOAD
-#define BOARD_AUTODLD_EN_PIN   CONFIG_BOARD_AUTODLD_EN_PIN
-#define BOARD_AUTODLD_BOOT_PIN CONFIG_BOARD_AUTODLD_BOOT_PIN
-static bool s_reset_trigger = false;
-#endif
 
 #define USB_RX_BUF_SIZE CONFIG_USB_RX_BUF_SIZE
 #define USB_TX_BUF_SIZE CONFIG_USB_TX_BUF_SIZE
@@ -66,31 +68,7 @@ volatile bool s_wait_reset = false;
 volatile bool s_in_boot = false;
 static RingbufHandle_t s_usb_tx_ringbuf = NULL;
 static RingbufHandle_t s_usb_rx_ringbuf = NULL;
-
-#ifdef CONFIG_UART_AUTO_DOWNLOAD
-static bool board_autodl_gpio_init(void)
-{
-    gpio_config_t io_conf = {0};
-    //disable interrupt
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    //set as output mode
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    //bit mask of the pins that you want to set
-    io_conf.pin_bit_mask = ((1ULL << BOARD_AUTODLD_EN_PIN) | (1ULL << BOARD_AUTODLD_BOOT_PIN));
-    //disable pull-down mode
-    io_conf.pull_down_en = 0;
-    //disable pull-up mode
-    io_conf.pull_up_en = GPIO_PULLUP_ENABLE;
-    //configure GPIO with the given settings
-    esp_err_t ret = gpio_config(&io_conf);
-
-    if (ret != ESP_OK) {
-        return false;
-    }
-
-    return true;
-}
-#endif
+static SemaphoreHandle_t s_trigger_bootloader = NULL;
 
 static bool board_zg23_reset_gpio_init(void)
 {
@@ -219,31 +197,6 @@ static void tinyusb_cdc_line_state_changed_callback(int itf, cdcacm_event_t *eve
 
     gpio_set_level(BOARD_ZG23_BTL_PIN, boot);
     gpio_set_level(BOARD_ZG23_RESET_PIN, reset);
-
-#ifdef CONFIG_UART_AUTO_DOWNLOAD
-    bool _io0 = true;
-    bool _en = true;
-    if (dtr == 1 && rts == 0) { //esptool ~100ms
-        _io0 = false;
-        _en = true;
-    }
-
-    if (dtr == 0 && rts == 1) {
-        _io0 = true;
-        _en = false;
-    }
-
-    s_reset_trigger = false;
-    if (dtr & rts) {
-        ESP_LOGW(TAG, "...");
-        s_reset_trigger = true;
-    } else {
-        ESP_LOGI(TAG, "DTR = %d, RTS = %d -> BOOT = %d, RST = %d", dtr, rts, _io0, _en);
-        gpio_set_level(BOARD_AUTODLD_BOOT_PIN, _io0);
-        gpio_set_level(BOARD_AUTODLD_EN_PIN, _en);
-    }
-#endif
-
 }
 
 static void tinyusb_cdc_line_coding_changed_callback(int itf, cdcacm_event_t *event)
@@ -253,6 +206,32 @@ static void tinyusb_cdc_line_coding_changed_callback(int itf, cdcacm_event_t *ev
     uint8_t parity = event->line_coding_changed_data.p_line_coding->parity;
     uint8_t data_bits = event->line_coding_changed_data.p_line_coding->data_bits;
     ESP_LOGV(TAG, "host require bit_rate=%" PRIu32 " stop_bits=%u parity=%u data_bits=%u", bit_rate, stop_bits, parity, data_bits);
+
+    static uint8_t magic_bootloader_trigger_stage = 0;
+    static uint64_t magic_bootloader_trigger_time = 0;
+
+    uint64_t now = esp_timer_get_time();
+
+    if ((now - magic_bootloader_trigger_time > MAGIC_BOOTLOADER_TRIGGER_TIMEOUT_MICROS) && magic_bootloader_trigger_stage > 0) {
+        ESP_LOGW(TAG, "Magic bootloader baudrate period timed out, resetting state");
+        magic_bootloader_trigger_stage = 0;
+    }
+
+    if (bit_rate == MAGIC_BOOTLOADER_TRIGGER_BAUDRATES[magic_bootloader_trigger_stage]) {
+        if (magic_bootloader_trigger_stage == 0) {
+            magic_bootloader_trigger_time = now;
+        }
+
+        ESP_LOGW(TAG, "Received magic bootloader baudrate, stage %d (%" PRIu32 " baud)", magic_bootloader_trigger_stage, bit_rate);
+        magic_bootloader_trigger_stage++;
+
+        if (magic_bootloader_trigger_stage == sizeof(MAGIC_BOOTLOADER_TRIGGER_BAUDRATES) / sizeof(MAGIC_BOOTLOADER_TRIGGER_BAUDRATES[0])) {
+            ESP_LOGW(TAG, "Launching bootloader reset task");
+            magic_bootloader_trigger_stage = 0;
+            xSemaphoreGive(s_trigger_bootloader);
+            return;
+        }
+    }
 
     if (s_baud_rate_active != bit_rate) {
         if (ESP_OK == uart_set_baudrate(BOARD_UART_PORT, CFG_BAUD_RATE(bit_rate))) {
@@ -441,6 +420,23 @@ static void uart_tx_task(void *arg) {
     }
 }
 
+static void bootloader_reset_task(void *arg) {
+    xSemaphoreTake(s_trigger_bootloader, portMAX_DELAY);
+
+    // Give the USB stack a little bit more time to finish up, this is triggered from an
+    // interrupt context
+    vTaskDelay(pdMS_TO_TICKS(500));
+
+    ESP_LOGW(TAG, "Uninstalling tinyUSB driver");
+    tinyusb_driver_uninstall();
+
+    vTaskDelay(pdMS_TO_TICKS(500));
+
+    ESP_LOGW(TAG, "Triggering bootloader");;
+    REG_WRITE(RTC_CNTL_OPTION1_REG, RTC_CNTL_FORCE_DOWNLOAD_BOOT);
+    esp_restart();
+}
+
 void app_main(void)
 {
     // Only for debugging - do not leave uncommented in production:
@@ -463,10 +459,9 @@ void app_main(void)
 
     board_zg23_reset_gpio_init();
 
-#ifdef CONFIG_UART_AUTO_DOWNLOAD
-    board_autodl_gpio_init();
-#endif
     s_usb_tx_ringbuf = xRingbufferCreate(USB_TX_BUF_SIZE, RINGBUF_TYPE_BYTEBUF);
+    s_trigger_bootloader = xSemaphoreCreateBinary();
+
     if (s_usb_tx_ringbuf == NULL) {
         ESP_LOGE(TAG, "USB TX buffer creation error");
         assert(0);
@@ -545,6 +540,7 @@ void app_main(void)
 
     xTaskCreate(usb_tx_task, "usb_tx", stack_size, &acm_cfg, 4, &usb_tx_handle);
     xTaskCreate(uart_tx_task, "uart_tx", stack_size, NULL, 4, NULL);
+    xTaskCreate(bootloader_reset_task, "bootloader_reset", 2048, NULL, 4, NULL);
 
     vTaskDelay(pdMS_TO_TICKS(500));
 
@@ -557,20 +553,6 @@ void app_main(void)
     ESP_LOGI(TAG, "USB initialization DONE");
 
     while (1) {
-#ifdef CONFIG_UART_AUTO_DOWNLOAD
-        static size_t trigger_reset = 0;
-        const size_t threshold_reset = 10;
-
-        if ((s_reset_trigger) && ++trigger_reset > threshold_reset) {
-            trigger_reset = 0;
-            s_reset_trigger = false;
-            gpio_set_level(BOARD_AUTODLD_BOOT_PIN, true);
-            gpio_set_level(BOARD_AUTODLD_EN_PIN, true);
-            ESP_LOGI(TAG, "DTR = %d, RTS = %d -> BOOT = %d, RST = %d", 1, 1, 1, 1);
-        } else {
-            trigger_reset = 0;
-        }
-#endif
-        vTaskDelay(pdMS_TO_TICKS(1));
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
