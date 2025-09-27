b154f81bd77a39c6b7edab86c500afafa2c00d9d
kbx81 <kbx81x@gmail.com>
2025-09-27 05:08:48 -0500

    Add EWT manifest generation

---
+++ build_ewt_manifest.py
@@ -0,0 +1,26 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', help='Version number to use in the manifest. Example: 2025.09.2', required=True, type=str)
+
+version = parser.parse_args().v
+
+manifest = {
+  "name": "zwa-2.usb=uart-bridge",
+  "version": version,
+  "home_assistant_domain": "esphome",
+  "new_install_prompt_erase": "false",
+  "builds": [
+    {
+      "chipFamily": "ESP32-S3",
+      "parts": [
+        {
+          "path": f"{version}/zwa2-esp-bridge.{version}.factory.bin",
+          "offset": 0
+        }
+      ]
+    }
+  ]
+}
+
+print(manifest)
